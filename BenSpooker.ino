34b52a2c884e15c6a52bf5a792fe32f2cb930e41
Joey Gore <joseph.gore37@gmail.com>
2019-02-25 20:12:44 -0500

    Plays Happy Birthday, Only plays loud tone for shock, synced LEDs to music, and shortened ultrasonic sensor to 250ms.

--- BenSpooker.ino
+++ BenSpooker.ino
@@ -15,6 +15,7 @@
 #define W_PIN 4
 #define B_PIN 5
 #define BKLT_PIN 6
+#define SPKR_PIN 7
 
 #define RS 8
 #define EN 9
@@ -27,9 +28,18 @@
 bool triggered;
 long duration;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+int leds[] = {R_PIN, W_PIN, B_PIN};
+int ledssize = sizeof(leds)/sizeof(*leds);
+int currentled;
+
+int length = 28; // the number of notes
+char notes[] = "GGAGcB GGAGdc GGxecBA yyecdc";
+int beats[] = { 2, 2, 8, 8, 8, 16, 1, 2, 2, 8, 8,8, 16, 1, 2,2,8,8,8,8,16, 1,2,2,8,8,8,16 };
+int tempo = 150;
 
 void setup() {
   triggered = false;
+  currentled = 0;
 
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
@@ -38,6 +48,7 @@ void setup() {
   pinMode(W_PIN, OUTPUT);
   pinMode(B_PIN, OUTPUT);
   pinMode(BKLT_PIN, OUTPUT);
+  pinMode(SPKR_PIN, OUTPUT);
 
   lcd.begin(16, 2);
   
@@ -48,7 +59,7 @@ void setup() {
 void loop() {
   // Do nothing until Ultrasonic sensor is triggered
   while(!triggered) {
-    delay(500);
+    delay(250);
     
     digitalWrite(TRIG_PIN, HIGH);
     delayMicroseconds(10);
@@ -63,20 +74,21 @@ void loop() {
     }
   }
 
-  //digitalWrite(BUZZER_PIN, HIGH);
-  digitalWrite(R_PIN, LOW);
-  digitalWrite(W_PIN, HIGH);
-  delay(500);
+  // Play Happy birthday
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo); // rest
+    } else {
+      playNote(notes[i], beats[i] * tempo);
+    }
 
-  //digitalWrite(BUZZER_PIN, LOW);
-  digitalWrite(W_PIN, LOW);
-  digitalWrite(B_PIN, HIGH);
-  delay(500);
-
-  //digitalWrite(BUZZER_PIN, HIGH);
-  digitalWrite(B_PIN, LOW);
-  digitalWrite(R_PIN, HIGH);
-  delay(500);
+   // pause between notes
+   delay(tempo);
+   digitalWrite(leds[currentled], LOW);
+   digitalWrite(leds[(currentled+1)%ledssize], HIGH);
+   currentled = (currentled+1)%ledssize;
+   digitalWrite(BUZZER_PIN, LOW);
+  }
 }
 
 void writeLCD() {
@@ -84,5 +96,34 @@ void writeLCD() {
   lcd.setCursor(0,1);
   lcd.print("BEN!!!!!!!!!!!!");
   digitalWrite(BKLT_PIN, HIGH);
+}
+
+void playTone(int tone, int toneDuration) {
+  for (long i = 0; i < toneDuration * 1000L; i += tone * 2) {
+    digitalWrite(SPKR_PIN, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(SPKR_PIN, LOW);
+    delayMicroseconds(tone);
+    }
+}
+
+void playNote(char note, int noteDuration) {
+  char names[] = {'C', 'D', 'E', 'F', 'G', 'A', 'B',           
+                  'c', 'd', 'e', 'f', 'g', 'a', 'b',
+                  'x', 'y' };
+
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014,
+                  956,  834,  765,  593,  468,  346,  224,
+                  655 , 715 };
+
+  int SPEE = 5;
+
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 17; i++) {
+    if (names[i] == note) {
+      int newduration = noteDuration/SPEE;
+      playTone(tones[i], newduration);
+    }
+  }
 }
 
